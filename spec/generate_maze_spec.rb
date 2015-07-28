72b240623efafbf032029a96b7f4d184323d0860
Josh Cheek <josh.cheek@gmail.com>
2015-07-27 22:06:09 -0600

    Pull maze generation out to its own file, write some spec descriptions for it

---
+++ spec/generate_maze_spec.rb
@@ -0,0 +1,12 @@
+require 'maze'
+
+RSpec.describe Maze::Generate do
+  it 'generates a maze with the given width, height'
+  it 'randomly chooses a start and end spot'
+  it 'invokes a callback for each cell it paves'
+
+  describe 'paving' do
+    it 'does not pave the edges of the maze'
+    it 'does not wind up with diagonally located paths'
+  end
+end
